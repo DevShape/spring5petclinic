dd4f3255762956d6d73907c57f394c056f76071d
DevShape <42915957+DevShape@users.noreply.github.com>
2021-06-14 20:47:57 +0200

    Adding Spring Config. Closes #21.

--- spring5petclinic-data/src/main/java/guru/springframework/spring5petclinic/services/map/PerServiceMap.java
+++ spring5petclinic-data/src/main/java/guru/springframework/spring5petclinic/services/map/PerServiceMap.java
@@ -2,9 +2,11 @@ package guru.springframework.spring5petclinic.services.map;
 
 import guru.springframework.spring5petclinic.model.Pet;
 import guru.springframework.spring5petclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PerServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findall() {
--- spring5petclinic-web/src/main/java/guru/springframework/spring5petclinic/bootstrap/DataLoader.java
+++ spring5petclinic-web/src/main/java/guru/springframework/spring5petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import guru.springframework.spring5petclinic.model.Owner;
 import guru.springframework.spring5petclinic.model.Vet;
 import guru.springframework.spring5petclinic.services.OwnerService;
 import guru.springframework.spring5petclinic.services.VetService;
-import guru.springframework.spring5petclinic.services.map.OwnerServiceMap;
-import guru.springframework.spring5petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +13,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
