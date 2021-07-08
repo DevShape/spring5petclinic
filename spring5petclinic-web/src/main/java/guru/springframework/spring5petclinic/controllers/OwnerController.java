1e2f17f2ede9251eac155be99951bc2c8e4ae515
DevShape <42915957+DevShape@users.noreply.github.com>
2021-07-08 16:15:26 +0200

    Fixed broken links . Closes #44.

--- spring5petclinic-web/src/main/java/guru/springframework/spring5petclinic/controllers/OwnerController.java
+++ spring5petclinic-web/src/main/java/guru/springframework/spring5petclinic/controllers/OwnerController.java
@@ -19,4 +19,9 @@ public class OwnerController {
         model.addAttribute("owners",ownerService.findall());
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
+    }
 }
